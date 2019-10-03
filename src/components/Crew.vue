3f0d2ae9e37f0628c08a5e2ba6e325c90472f313
David M McPeak <mcpeakdm@MBP-DMPEAK.local>
2019-10-03 16:03:27 -0400

    Updated location and student technicians

--- src/components/Crew.vue
+++ src/components/Crew.vue
@@ -3,10 +3,10 @@
     <h1>Meet the members of Signal Team!</h1>
     <section>
       <h2 class="nich">Nich Carpenter- Senior Technician</h2>
-      <!--<h2 class="malone">Malone Weghorst- Senior Technician</h2>-->
       <h3 class="name1">Amy Kifer- Student Technician</h3>
       <h3 class="names">Stephan Padilla- Student Technician</h3>
-      <h3 class="names">Briana Stauffaur- Student Technician</h3>
+      <h3 class="names">Joshua Knapp- Student Technician</h3>
+      <h3 class="names">Noah Warner- Student Technician</h3>
     </section>
   </v-container>
 </template>
